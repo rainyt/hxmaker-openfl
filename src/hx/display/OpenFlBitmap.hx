de78fd4a7e6a0ba182e3dd813ce47793acf78147
Rainy <939029835@qq.com>
2025-01-22 15:47:12 +0800

    改进OpenFLBitmap的实现

--- src/hx/display/OpenFlBitmap.hx
+++ src/hx/display/OpenFlBitmap.hx
@@ -1,5 +1,6 @@
 package hx.display;
 
+import hx.gemo.Rectangle;
 import openfl.display.Bitmap;
 
 /**
@@ -14,5 +15,29 @@ class OpenFlBitmap extends CustomDisplayObject {
 	public function new(bitmapData:BitmapData) {
 		bitmap = new Bitmap(bitmapData.data?.getTexture());
 		super(bitmap);
+	}
+
+	override function get_width():Float {
+		if (__width != null) {
+			return __width;
+		}
+		return super.get_width();
+	}
+
+	override function get_height():Float {
+		if (__height != null) {
+			return __height;
+		}
+		return getBounds().height;
+	}
+
+	override function __getRect():Rectangle {
+		if (root == null) {
+			__rect.width = __rect.height = 0;
+		} else {
+			__rect.width = bitmap.bitmapData != null ? bitmap.bitmapData.width : 0;
+			__rect.height = bitmap.bitmapData != null ? bitmap.bitmapData.height : 0;
+		}
+		return __rect;
 	}
 }
