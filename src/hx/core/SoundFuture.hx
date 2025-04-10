4c969e61b9774cd733fba7342d2df15d769ca944
Rainy <939029835@qq.com>
2025-04-10 21:23:35 +0800

    支持音频自定义加载

--- src/hx/core/SoundFuture.hx
+++ src/hx/core/SoundFuture.hx
@@ -1,5 +1,8 @@
 package hx.core;
 
+import openfl.events.IOErrorEvent;
+import openfl.events.Event;
+import openfl.net.URLRequest;
 import hx.events.FutureErrorEvent;
 import openfl.Assets;
 import hx.assets.Sound;
@@ -9,15 +12,19 @@ class SoundFuture extends Future<Sound, String> {
 	override function post() {
 		super.post();
 		var path = getLoadData();
-        #if cpp
+		#if cpp
 		path = StringTools.replace(path, ".mp3", ".ogg");
-        #end
-		Assets.loadSound(path).onComplete((sound) -> {
+		#end
+		var url = new URLRequest(path);
+		var sound = new openfl.media.Sound();
+		sound.addEventListener(Event.COMPLETE, (e) -> {
 			var data = new Sound();
 			data.root = new OpenFLSound(sound);
 			this.completeValue(data);
-		}).onError(err -> {
+		});
+		sound.addEventListener(IOErrorEvent.IO_ERROR, (e) -> {
 			this.errorValue(FutureErrorEvent.create(FutureErrorEvent.LOAD_ERROR, -1, "load fail:" + this.getLoadData()));
 		});
+		sound.load(url);
 	}
 }
