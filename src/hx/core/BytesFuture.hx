e4a5df20a3a3fd57d07cc95c18421ccf387067d4
Rainy <939029835@qq.com>
2025-06-30 17:46:31 +0800

    调整为URLLoader加载方式

--- src/hx/core/BytesFuture.hx
+++ src/hx/core/BytesFuture.hx
@@ -1,5 +1,10 @@
 package hx.core;
 
+import hx.events.FutureErrorEvent;
+import openfl.events.IOErrorEvent;
+import openfl.events.Event;
+import openfl.net.URLRequest;
+import openfl.net.URLLoader;
 import haxe.io.Bytes;
 import openfl.Assets;
 import hx.assets.Future;
@@ -7,8 +12,14 @@ import hx.assets.Future;
 class BytesFuture extends Future<Bytes, String> {
 	override function post() {
 		super.post();
-		Assets.loadBytes(getLoadData()).onComplete((bytes) -> {
-			this.completeValue(bytes);
-		}).onError(this.errorValue);
+		var urlLoader = new URLLoader();
+		urlLoader.dataFormat = BINARY;
+		urlLoader.addEventListener(Event.COMPLETE, (e) -> {
+			this.completeValue(urlLoader.data);
+		});
+		urlLoader.addEventListener(IOErrorEvent.IO_ERROR, (e) -> {
+			this.errorValue(new FutureErrorEvent(FutureErrorEvent.LOAD_ERROR, false, false));
+		});
+		urlLoader.load(new URLRequest(getLoadData()));
 	}
 }
