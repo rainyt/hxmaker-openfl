2f9b8c82fd4966fc12bd0dd8fbe3605bbd19b7d2
Rainy <939029835@qq.com>
2025-12-24 09:57:01 +0800

    抽象BlendMode过滤器

---
+++ src/hx/filters/BlendModeFilter.hx
@@ -0,0 +1,30 @@
+package hx.filters;
+
+import hx.shader.MultiTextureShader;
+import hx.shader.SubtractShader;
+import hx.core.OpenFlBitmapData;
+import hx.core.Hxmaker;
+import hx.display.Image;
+import hx.display.DisplayObject;
+
+/**
+ * 混合模式滤镜
+ */
+class BlendModeFilter extends StageBitmapRenderFilter {
+	public var displayImage:Image = new Image();
+
+	override function init() {
+		super.init();
+		displayImage.data = OpenFlBitmapData.fromSize(Std.int(Hxmaker.engine.stageWidth), Std.int(Hxmaker.engine.stageHeight), true, 0x0);
+	}
+
+	override function update(display:DisplayObject, dt:Float) {
+		super.update(display, dt);
+		// 渲染舞台的画面位图
+		this.bitmapData.clear();
+		Hxmaker.engine.renderer.renderToBitmapData(bitmapData);
+		displayImage.data.clear();
+		displayImage.data.draw(display);
+		cast(this.render.shader, MultiTextureShader).uSampler1.input = displayImage.data.data.getTexture();
+	}
+}
