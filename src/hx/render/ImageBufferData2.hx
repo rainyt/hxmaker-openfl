fc52cb4a87c42fc5522c8c5ed09584aae7bba2dc
Rainy <939029835@qq.com>
2026-03-28 10:25:34 +0800

    保留副本

---
+++ src/hx/render/ImageBufferData2.hx
@@ -0,0 +1,565 @@
+package hx.render;
+
+import hx.utils.ContextStats;
+import hx.geom.Matrix3D;
+import hx.shader.MultiTextureShader;
+import hx.display.DisplayObject;
+import hx.display.BlendMode;
+import hx.core.OpenFlBitmapData;
+import hx.geom.Matrix;
+import hx.display.Graphics;
+import hx.core.Render;
+import openfl.display.BitmapData;
+import hx.display.Image;
+import openfl.Vector;
+import openfl.utils._internal.Float32Array;
+
+/**
+ * 图片缓存数据
+ */
+@:access(hx.display.Image)
+@:access(hx.display.Graphics)
+@:access(hx.geom.Matrix)
+class ImageBufferData2 {
+	/**
+	 * 只有1像素的位图
+	 */
+	public static var px1bitmapData(get, never):hx.display.BitmapData;
+
+	private static var __px1bitmapData:hx.display.BitmapData;
+
+	private static function get_px1bitmapData():hx.display.BitmapData {
+		if (__px1bitmapData == null) {
+			__px1bitmapData = hx.display.BitmapData.formData(new OpenFlBitmapData(new BitmapData(1, 1, false, 0xffffff)));
+		}
+		return __px1bitmapData;
+	}
+
+	/**
+	 * 纹理ID顶点列表
+	 */
+	public var ids:Array<Float> = [];
+
+	/**
+	 * 透明度渲染
+	 */
+	public var alphas:Array<Float> = [];
+
+	/**
+	 * 叠加渲染支持
+	 */
+	public var addBlendModes:Array<Float> = [];
+
+	/**
+	 * 是否包含颜色转换
+	 */
+	public var hasColorTransform:Array<Float> = [];
+
+	/**
+	 * 颜色相乘
+	 */
+	public var colorMultiplier:Array<Float> = [];
+
+	/**
+	 * 颜色偏移
+	 */
+	public var colorOffset:Array<Float> = [];
+
+	/**
+	 * 纹理顶点坐标
+	 */
+	public var vertices:Vector<Float> = new Vector();
+
+	/**
+	 * 纹理顶点索引
+	 */
+	public var indices:Vector<Int> = new Vector();
+
+	/**
+	 * 纹理顶点UV
+	 */
+	public var uvtData:Vector<Float> = new Vector();
+
+	/**
+	 * 待渲染纹理
+	 */
+	public var bitmapDatas:Array<BitmapData> = [];
+
+	/**
+	 * 渲染列表
+	 */
+	public var drawDisplayList:Array<DisplayObject> = [];
+
+	/**
+	 * 纹理ID映射表
+	 */
+	public var mapIds:Map<BitmapData, Int> = [];
+
+	/**
+	 * 平滑
+	 */
+	public var smoothing:Bool = false;
+
+	/**
+	 * 渲染模式
+	 */
+	public var blendMode:BlendMode = null;
+
+	/**
+	 * 数据缓存是否已经损坏
+	 */
+	public var isBad:Bool = false;
+
+	/**
+	 * 数据索引
+	 */
+	public var index = 0;
+
+	/**
+	 * 每个顶点缓冲区所需的数据大小
+	 */
+	public var perBufferSize(get, never):Int;
+
+	private function get_perBufferSize():Int {
+		/**
+			attribute float openfl_Alpha_multi;
+			attribute vec4 openfl_ColorMultiplier_muti;
+			attribute vec4 openfl_ColorOffset_muti;
+			attribute vec4 openfl_Position;
+			attribute vec2 openfl_TextureCoord;
+			attribute float openfl_TextureId;
+			attribute float openfl_HasColorTransform_muti;
+			attribute float openfl_blendMode_add;
+
+			uniform mat4 openfl_Matrix;
+			uniform vec2 openfl_TextureSize;
+			uniform float time;
+
+		 */
+		return Std.int(this.vertices.length / 2) * perBufferCounts;
+	}
+
+	/**
+	 * 每个顶点缓冲区所需的数据数量
+	 */
+	public var perBufferCounts(get, never):Int;
+
+	private function get_perBufferCounts():Int {
+		return (1 + 4 + 4 + 4 + 2 + 1 + 1 + 1);
+	}
+
+	/**
+	 * 缓冲区数据
+	 */
+	private var __buffer:Float32Array;
+
+	/**
+	 * 构造缓冲数据，主要为`NativeMultiTextureShader`使用
+	 */
+	public function buildBuffer():Void {
+		var bufferSize = perBufferSize;
+		__buffer = new Float32Array(bufferSize);
+		// if (__buffer == null) {
+		// } else if (bufferSize > __buffer.length) {
+		// __buffer = new Float32Array(bufferSize);
+		// }
+		var bufferCounts = perBufferCounts;
+		var counts = Std.int(vertices.length / 2);
+		for (i in 0...counts) {
+			// openfl_Alpha_multi
+			__buffer[i * bufferCounts] = alphas[i];
+			// openfl_ColorMultiplier_muti
+			__buffer[i * bufferCounts + 1] = hasColorTransform[i] == 1 ? colorMultiplier[i * 4] : 0;
+			__buffer[i * bufferCounts + 2] = hasColorTransform[i] == 1 ? colorMultiplier[i * 4 + 1] : 0;
+			__buffer[i * bufferCounts + 3] = hasColorTransform[i] == 1 ? colorMultiplier[i * 4 + 2] : 0;
+			__buffer[i * bufferCounts + 4] = hasColorTransform[i] == 1 ? colorMultiplier[i * 4 + 3] : 0;
+			// openfl_ColorOffset_muti
+			__buffer[i * bufferCounts + 5] = hasColorTransform[i] == 1 ? colorOffset[i * 4] : 0;
+			__buffer[i * bufferCounts + 6] = hasColorTransform[i] == 1 ? colorOffset[i * 4 + 1] : 0;
+			__buffer[i * bufferCounts + 7] = hasColorTransform[i] == 1 ? colorOffset[i * 4 + 2] : 0;
+			__buffer[i * bufferCounts + 8] = hasColorTransform[i] == 1 ? colorOffset[i * 4 + 3] : 0;
+			// openfl_Position
+			__buffer[i * bufferCounts + 9] = vertices[i * 2];
+			__buffer[i * bufferCounts + 10] = vertices[i * 2 + 1];
+			__buffer[i * bufferCounts + 11] = 0;
+			__buffer[i * bufferCounts + 12] = 1;
+			// openfl_TextureCoord
+			__buffer[i * bufferCounts + 13] = uvtData[i * 2];
+			__buffer[i * bufferCounts + 14] = uvtData[i * 2 + 1];
+			// openfl_TextureId
+			__buffer[i * bufferCounts + 15] = ids[i];
+			// openfl_HasColorTransform_muti
+			__buffer[i * bufferCounts + 16] = hasColorTransform[i];
+			// openfl_blendMode_add
+			__buffer[i * bufferCounts + 17] = addBlendModes[i];
+		}
+	}
+
+	private var dataPerVertex6 = 0;
+	private var dataPerVertex24 = 0;
+	private var dataPerVertex = 0;
+	private var indicesOffset = 0;
+
+	public function new() {}
+
+	public function reset() {
+		index = 0;
+		dataPerVertex6 = 0;
+		dataPerVertex24 = 0;
+		dataPerVertex = 0;
+		indicesOffset = 0;
+		setArrayLength(bitmapDatas, 0);
+		mapIds.clear();
+		isBad = false;
+		blendMode = null;
+	}
+
+	public function endFill():Void {
+		vertices.length = dataPerVertex;
+		indices.length = dataPerVertex6;
+		setArrayLength(ids, dataPerVertex6);
+		setArrayLength(alphas, dataPerVertex6);
+		setArrayLength(hasColorTransform, dataPerVertex6);
+		setArrayLength(colorMultiplier, dataPerVertex24);
+		setArrayLength(colorOffset, dataPerVertex24);
+		setArrayLength(drawDisplayList, index);
+		uvtData.length = dataPerVertex;
+	}
+
+	private function setArrayLength(array:Array<Dynamic>, length:Int):Void {
+		while (array.length > length) {
+			array.pop();
+		}
+	}
+
+	/**
+	 * 绘制图形
+	 * @param graphic 
+	 * @param render 
+	 * @return Bool
+	 */
+	public function drawGraphic(graphic:Graphics, render:Render):Bool {
+		var data = graphic.__graphicsDrawData;
+		var len = data.draws.length;
+		while (graphic.__graphicsDrawData.index < len) {
+			var command = data.draws[data.index];
+			if (command == null) {
+				// 当命令为空时，则意味着渲染已结束
+				break;
+			}
+			switch command {
+				case BEGIN_FILL(color):
+					data.currentBitmapData = px1bitmapData;
+					data.smoothing = true;
+				case BEGIN_BITMAP_DATA(bitmapData, smoothing):
+					data.currentBitmapData = bitmapData;
+					data.smoothing = smoothing;
+				case DRAW_TRIANGLE(vertices, indices, uvs, alpha, colorTransform, applyBlendAddMode):
+					// 开始绘制三角形
+					if (data.currentBitmapData != null) {
+						var texture = data.currentBitmapData.data.getTexture();
+						if (index == 0 || !mapIds.exists(texture)) {
+							if (bitmapDatas.length >= MultiTextureShader.supportedMultiTextureUnits) {
+								return false;
+							}
+						}
+						// 如果平滑值不同，则产生新的绘制
+						if (index == 0) {
+							smoothing = data.smoothing;
+							blendMode = graphic.blendMode;
+						} else if (blendMode != graphic.blendMode) {
+							if (blendMode == ADD || blendMode == NORMAL) {
+								if (graphic.blendMode != ADD && graphic.blendMode != NORMAL) {
+									return false;
+								}
+							} else {
+								return false;
+							}
+						} else if (smoothing != data.smoothing) {
+							return false;
+						}
+						if (!applyBlendAddMode && graphic.blendMode == ADD) {
+							applyBlendAddMode = true;
+						}
+						// 可以绘制，记录纹理ID
+						var id = mapIds.get(texture);
+						if (id == null) {
+							bitmapDatas.push(texture);
+							id = bitmapDatas.length - 1;
+							mapIds.set(texture, id);
+						}
+						// 根据顶点设置数据
+						for (i in 0...indices.length) {
+							ids[dataPerVertex6 + i] = id;
+							alphas[dataPerVertex6 + i] = graphic.__worldAlpha * alpha;
+							addBlendModes[dataPerVertex6 + i] = applyBlendAddMode ? 1 : 0;
+							if (colorTransform != null) {
+								hasColorTransform[dataPerVertex6 + i] = 1;
+								colorMultiplier[dataPerVertex24 + i * 4] = colorTransform.redMultiplier;
+								colorMultiplier[dataPerVertex24 + i * 4 + 1] = colorTransform.greenMultiplier;
+								colorMultiplier[dataPerVertex24 + i * 4 + 2] = colorTransform.blueMultiplier;
+								colorMultiplier[dataPerVertex24 + i * 4 + 3] = colorTransform.alphaMultiplier;
+								colorOffset[dataPerVertex24 + i * 4] = colorTransform.redOffset;
+								colorOffset[dataPerVertex24 + i * 4 + 1] = colorTransform.greenOffset;
+								colorOffset[dataPerVertex24 + i * 4 + 2] = colorTransform.blueOffset;
+								colorOffset[dataPerVertex24 + i * 4 + 3] = colorTransform.alphaOffset;
+							} else {
+								hasColorTransform[dataPerVertex6 + i] = 0;
+								if (graphic.colorTransform != null) {
+									colorMultiplier[dataPerVertex24 + i * 4] = 1;
+									colorMultiplier[dataPerVertex24 + i * 4 + 1] = 1;
+									colorMultiplier[dataPerVertex24 + i * 4 + 2] = 1;
+									colorMultiplier[dataPerVertex24 + i * 4 + 3] = 1;
+									colorOffset[dataPerVertex24 + i * 4] = 0;
+									colorOffset[dataPerVertex24 + i * 4 + 1] = 0;
+									colorOffset[dataPerVertex24 + i * 4 + 2] = 0;
+									colorOffset[dataPerVertex24 + i * 4 + 3] = 0;
+								} else {
+									colorOffset[dataPerVertex24 + i * 4 + 3] = 0;
+								}
+							}
+							if (graphic.colorTransform != null) {
+								hasColorTransform[dataPerVertex6 + i] = 1;
+								colorMultiplier[dataPerVertex24 + i * 4] *= graphic.colorTransform.redMultiplier;
+								colorMultiplier[dataPerVertex24 + i * 4 + 1] *= graphic.colorTransform.greenMultiplier;
+								colorMultiplier[dataPerVertex24 + i * 4 + 2] *= graphic.colorTransform.blueMultiplier;
+								colorMultiplier[dataPerVertex24 + i * 4 + 3] *= graphic.colorTransform.alphaMultiplier;
+								colorOffset[dataPerVertex24 + i * 4] += graphic.colorTransform.redOffset;
+								colorOffset[dataPerVertex24 + i * 4 + 1] += graphic.colorTransform.greenOffset;
+								colorOffset[dataPerVertex24 + i * 4 + 2] += graphic.colorTransform.blueOffset;
+								colorOffset[dataPerVertex24 + i * 4 + 3] += graphic.colorTransform.alphaOffset;
+							}
+							this.indices[dataPerVertex6 + i] = indicesOffset + indices[i];
+						}
+
+						// 顶点坐标
+						var tileTransform:Matrix = @:privateAccess graphic.__worldTransform;
+						var len = Std.int(vertices.length / 2);
+						for (i in 0...len) {
+							var x = vertices[i * 2];
+							var y = vertices[i * 2 + 1];
+							this.vertices[dataPerVertex + i * 2] = tileTransform.__transformX(x, y);
+							this.vertices[dataPerVertex + i * 2 + 1] = tileTransform.__transformY(x, y);
+							this.uvtData[dataPerVertex + i * 2] = uvs[i * 2];
+							this.uvtData[dataPerVertex + i * 2 + 1] = uvs[i * 2 + 1];
+						}
+
+						dataPerVertex6 += indices.length;
+						dataPerVertex24 += indices.length * 4;
+						dataPerVertex += vertices.length;
+						this.indicesOffset = Std.int(dataPerVertex / 2);
+						this.index++;
+						// 写入
+						drawDisplayList[index] = graphic;
+						graphic.__transformDirty = false;
+						graphic.__colorTransformDirty = false;
+						graphic.__uvsDirty = false;
+						this.isBad = true;
+					}
+			}
+			data.index++;
+		}
+		graphic.__graphicsDrawData.index = 0;
+		ContextStats.statsGraphicRenderCount();
+		return true;
+	}
+
+	/**
+	 * 绘制图片
+	 * @param image 
+	 */
+	public function draw(image:Image, render:Render):Bool {
+		var texture = image.data.data.getTexture();
+		var id = mapIds.get(texture);
+		if (id == null && bitmapDatas.length >= MultiTextureShader.supportedMultiTextureUnits) {
+			return false;
+		}
+		// 如果平滑值不同，则产生新的绘制
+		if (index == 0) {
+			smoothing = image.smoothing;
+			blendMode = image.blendMode;
+		} else if (blendMode != image.blendMode) {
+			if (blendMode == ADD || blendMode == NORMAL) {
+				if (image.blendMode != ADD && image.blendMode != NORMAL) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} else if (smoothing != image.smoothing) {
+			return false;
+		}
+		// 可以绘制，记录纹理ID
+		if (id == null) {
+			bitmapDatas.push(texture);
+			id = bitmapDatas.length - 1;
+			mapIds.set(texture, id);
+		}
+
+		if (!isBad) {
+			var displayObject = drawDisplayList[index];
+			var isSame = displayObject == image;
+			if (!isSame)
+				isBad = true;
+		}
+		// var isColorDirty = isBad || image.__colorTransformDirty;
+		var isTransformDirty = isBad || image.__transformDirty;
+
+		// TODO 如果是一样的列表，只是设置visible，那么该脏检测不正确
+		// var isUvsDirty = isBad || image.__uvsDirty;
+		var isUvsDirty = true;
+		// TODO 同isUvsDirty，需要想办法优化解决所有可能发生的情况
+		var isColorDirty = true;
+
+		// if (displayObject != image || image.__transformDirty) {
+		if (isColorDirty) {
+			// 6个顶点数据
+			for (i in 0...6) {
+				ids[dataPerVertex6 + i] = id;
+				alphas[dataPerVertex6 + i] = image.__worldAlpha;
+				addBlendModes[dataPerVertex6 + i] = image.__addBlendMode;
+				if (image.__colorTransform != null) {
+					hasColorTransform[dataPerVertex6 + i] = 1;
+					colorMultiplier[dataPerVertex24 + i * 4] = image.__colorTransform.redMultiplier;
+					colorMultiplier[dataPerVertex24 + i * 4 + 1] = image.__colorTransform.greenMultiplier;
+					colorMultiplier[dataPerVertex24 + i * 4 + 2] = image.__colorTransform.blueMultiplier;
+					colorMultiplier[dataPerVertex24 + i * 4 + 3] = image.__colorTransform.alphaMultiplier;
+					colorOffset[dataPerVertex24 + i * 4] = image.__colorTransform.redOffset;
+					colorOffset[dataPerVertex24 + i * 4 + 1] = image.__colorTransform.greenOffset;
+					colorOffset[dataPerVertex24 + i * 4 + 2] = image.__colorTransform.blueOffset;
+					colorOffset[dataPerVertex24 + i * 4 + 3] = image.__colorTransform.alphaOffset;
+				} else {
+					hasColorTransform[dataPerVertex6 + i] = 0;
+					colorOffset[dataPerVertex24 + i * 4 + 3] = 0;
+				}
+			}
+		}
+
+		// if (isTransformDirty) {
+		// 坐标顶点
+		var tileWidth:Float = image.data.rect != null ? image.data.rect.width : image.data.data.getWidth();
+		var tileHeight:Float = image.data.rect != null ? image.data.rect.height : image.data.data.getHeight();
+		var tileTransform = @:privateAccess image.__worldTransform;
+		var x = @:privateAccess tileTransform.__transformX(0, 0);
+		var y = @:privateAccess tileTransform.__transformY(0, 0);
+		var x2 = @:privateAccess tileTransform.__transformX(tileWidth, 0);
+		var y2 = @:privateAccess tileTransform.__transformY(tileWidth, 0);
+		var x3 = @:privateAccess tileTransform.__transformX(0, tileHeight);
+		var y3 = @:privateAccess tileTransform.__transformY(0, tileHeight);
+		var x4 = @:privateAccess tileTransform.__transformX(tileWidth, tileHeight);
+		var y4 = @:privateAccess tileTransform.__transformY(tileWidth, tileHeight);
+
+		// 3D变化支持
+		var __transformMatrix3D = @:privateAccess image.__transformMatrix3D;
+		if (__transformMatrix3D.transform3D != null) {
+			var matrix3D = new Matrix3D();
+			matrix3D.identity();
+			matrix3D.appendTranslation(-tileTransform.tx, -tileTransform.ty, 0);
+			if (__transformMatrix3D.center3DVector != null)
+				matrix3D.appendTranslation(-__transformMatrix3D.center3DVector.x, -__transformMatrix3D.center3DVector.y, -__transformMatrix3D.center3DVector.z);
+			matrix3D.append(__transformMatrix3D.transform3D);
+
+			// if (__transformMatrix3D.projectionMatrix3D != null) {
+			// 	matrix3D.appendTranslation(image.stage.stageWidth / 2, 0, 400);
+			// 	matrix3D.append(__transformMatrix3D.projectionMatrix3D);
+			// }
+
+			// if (__transformMatrix3D.projectionMatrix3D != null) {
+			// matrix3D.appendTranslation(0, 0, 1000);
+			// matrix3D.append(__transformMatrix3D.projectionMatrix3D);
+			// }
+
+			if (__transformMatrix3D.center3DVector != null)
+				matrix3D.appendTranslation(__transformMatrix3D.center3DVector.x, __transformMatrix3D.center3DVector.y, __transformMatrix3D.center3DVector.z);
+			matrix3D.appendTranslation(tileTransform.tx, tileTransform.ty, 0);
+
+			// if (__transformMatrix3D.projectionMatrix3D != null) {
+			// 	matrix3D.appendTranslation(image.stage.stageWidth / 2, image.stage.stageHeight / 2, 1000);
+			// 	matrix3D.append(__transformMatrix3D.projectionMatrix3D);
+			// }
+
+			var array = [x, y, 0, x2, y2, 0, x3, y3, 0, x4, y4, 0];
+			// var array = [0, 0, tileWidth, 0, 0, tileWidth, tileWidth, tileHeight];
+			var projected = [];
+			var uvt = [];
+			Utils3D.projectVectors2D(matrix3D, array, projected, uvt);
+			// trace(projected);
+			x = projected[0];
+			y = projected[1];
+			x2 = projected[2];
+			y2 = projected[3];
+			x3 = projected[4];
+			y3 = projected[5];
+			x4 = projected[6];
+			y4 = projected[7];
+
+			// x = @:privateAccess tileTransform.__transformX(x, y);
+			// y = @:privateAccess tileTransform.__transformY(x, y);
+			// x2 = @:privateAccess tileTransform.__transformX(x2, y2);
+			// y2 = @:privateAccess tileTransform.__transformY(x2, y2);
+			// x3 = @:privateAccess tileTransform.__transformX(x3, y3);
+			// y3 = @:privateAccess tileTransform.__transformY(x3, y3);
+			// x4 = @:privateAccess tileTransform.__transformX(x4, y4);
+			// y4 = @:privateAccess tileTransform.__transformY(x4, y4);
+		}
+
+		vertices[dataPerVertex] = x;
+		vertices[dataPerVertex + 1] = y;
+		vertices[dataPerVertex + 2] = (x2);
+		vertices[dataPerVertex + 3] = (y2);
+		vertices[dataPerVertex + 4] = (x3);
+		vertices[dataPerVertex + 5] = (y3);
+		vertices[dataPerVertex + 6] = (x4);
+		vertices[dataPerVertex + 7] = (y4);
+
+		// 顶点
+		indices[dataPerVertex6] = (indicesOffset);
+		indices[dataPerVertex6 + 1] = (indicesOffset + 1);
+		indices[dataPerVertex6 + 2] = (indicesOffset + 2);
+		indices[dataPerVertex6 + 3] = (indicesOffset + 1);
+		indices[dataPerVertex6 + 4] = (indicesOffset + 2);
+		indices[dataPerVertex6 + 5] = (indicesOffset + 3);
+		// }
+
+		// UVs
+		if (isUvsDirty) {
+			if (image.data.rect != null) {
+				var imageWidth = image.data.data.getWidth();
+				var imageHeight = image.data.data.getHeight();
+				var uvX = image.data.rect.x / imageWidth;
+				var uvY = image.data.rect.y / imageHeight;
+				var uvW = (image.data.rect.x + image.data.rect.width) / imageWidth;
+				var uvH = (image.data.rect.y + image.data.rect.height) / imageHeight;
+				uvtData[dataPerVertex] = (uvX);
+				uvtData[dataPerVertex + 1] = (uvY);
+				uvtData[dataPerVertex + 2] = (uvW);
+				uvtData[dataPerVertex + 3] = (uvY);
+				uvtData[dataPerVertex + 4] = (uvX);
+				uvtData[dataPerVertex + 5] = (uvH);
+				uvtData[dataPerVertex + 6] = (uvW);
+				uvtData[dataPerVertex + 7] = (uvH);
+			} else {
+				uvtData[dataPerVertex] = (0);
+				uvtData[dataPerVertex + 1] = (0);
+				uvtData[dataPerVertex + 2] = (1);
+				uvtData[dataPerVertex + 3] = (0);
+				uvtData[dataPerVertex + 4] = (0);
+				uvtData[dataPerVertex + 5] = (1);
+				uvtData[dataPerVertex + 6] = (1);
+				uvtData[dataPerVertex + 7] = (1);
+			}
+		}
+		// }
+		drawDisplayList[index] = image;
+		image.__transformDirty = false;
+		image.__colorTransformDirty = false;
+		image.__uvsDirty = false;
+		// 下一个
+		index++;
+		dataPerVertex6 += 6;
+		dataPerVertex24 += 24;
+		dataPerVertex += 8;
+		indicesOffset += 4;
+		return true;
+	}
+}
