5045e7b0fcaf74072a5e07f747f5f0a5e88699c4
Rainy <939029835@qq.com>
2026-04-02 15:24:30 +0800

    无需使用这种方式

--- src/hx/core/EngineSprite.hx
+++ src/hx/core/EngineSprite.hx
@@ -8,21 +8,7 @@ import openfl.display.Sprite;
 class EngineSprite extends Sprite {
 	public var isPool = true;
 
-	/**
-	 * 是否使用原生多纹理着色器
-	 */
-	public var useNativeMultiTextureShader = false;
-
 	public function new() {
 		super();
-		this.addEventListener(RenderEvent.RENDER_OPENGL, onRenderOpenGL);
-	}
-
-	private function onRenderOpenGL(event:RenderEvent) {
-		if (useNativeMultiTextureShader) {
-			// 渲染OpenGL
-			var opengl:OpenGLRenderer = cast event.renderer;
-			var gl = opengl.gl;
-		}
 	}
 }
